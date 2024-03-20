3648c38041fef33d61c16661c01283ca77b2192e
Mangui <antonio.mangado@gmail.com>
2024-03-20 12:48:02 +0100

    ADD/ convertions are now rendered, and deleted properly

--- client/src/components/Coverter/CoverterForm/CoverterForm.jsx
+++ client/src/components/Coverter/CoverterForm/CoverterForm.jsx
@@ -1,24 +1,36 @@
-import React, {useState, useEffect} from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { addConvertion } from "../../../redux/slices/SavedConvertionsSlice";
+import {useState, useEffect} from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { addConvertion } from "../../../redux/slices/SavedConvertionsSlice";
 import UnitSelector from "./UnitSelector";
 import QuantityInput from "./QuantityInput";
 import FavButton from "./FavButton";
 import Result from "../CoverterForm/QuantityInput/Result";
+import axios from "../../../api/axios"
+const POST_URL = "api/convertions"
 const conversions = [0.621371, 1.60934, 3.28084, 0.3048, 0.393701, 2.54]
 
 const CoverterForm = () => {
 
-  const dispatch = useDispatch();
-  const savedConvertions = useSelector((state) => state.savedConvertions.convertions);
-
+  // const dispatch = useDispatch();
+  
   const [inputUnit, setInputUnit] = useState("kms");
   const [outputUnit, setOutputUnit] = useState("miles");
   const [arrayPosition, setArrayPosition] = useState(0);
   const [inputQuantity, setInputQuantity] = useState(0);
   const [outputQuantity, setOutputQuantity] = useState(0);
 
-  const addConvertionToStore = () => {
+  // const addConvertionToStore = () => {
+  //   const convertion = {
+  //     inputUnit: inputUnit,
+  //     outputUnit: outputUnit,
+  //     inputQuantity: inputQuantity,
+  //     outputQuantity: outputQuantity,
+  //     id: Date.now(),
+  //   };
+  //   dispatch(addConvertion(convertion));
+  // }
+
+  const addConvertionToDB = async () => {
     const convertion = {
       inputUnit: inputUnit,
       outputUnit: outputUnit,
@@ -26,9 +38,12 @@ const CoverterForm = () => {
       outputQuantity: outputQuantity,
       id: Date.now(),
     };
-    dispatch(addConvertion(convertion));
+    const response = await axios.post(POST_URL, JSON.stringify(convertion), {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    
+    location.reload()
   }
-  
 
   useEffect(() => {
     setOutputQuantity(inputQuantity * conversions[arrayPosition]);
@@ -42,7 +57,7 @@ const CoverterForm = () => {
         <QuantityInput inputUnit={inputUnit} setInputQuantity={setInputQuantity}/>
       </div>
       <div id="fav-and-result">
-        <FavButton addConvertion={addConvertionToStore}/>
+        <FavButton addConvertion={addConvertionToDB}/>
         <Result outputUnit={outputUnit} outputQuantity={outputQuantity}/>
       </div>
     </article>
--- client/src/components/Coverter/SavedConvertions/SavedConvertions.jsx
+++ client/src/components/Coverter/SavedConvertions/SavedConvertions.jsx
@@ -1,28 +1,54 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { removeConvertion } from "../../../redux/slices/SavedConvertionsSlice";
+import { useEffect, useState } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { removeConvertion } from "../../../redux/slices/SavedConvertionsSlice";
+import axios from "../../../api/axios";
 
 const SavedConvertions = () => {
 
-  const dispatch = useDispatch();
-  const savedConvertions = useSelector((state) => state.savedConvertions.convertions);
+  // const dispatch = useDispatch();
+  // const savedConvertions = useSelector((state) => state.savedConvertions.convertions);
 
-  const printCards = () => {
-    return savedConvertions.map((convertion, i) => {
+  
+  // const printCards = () => {
+    //   return savedConvertions.map((convertion) => {
+      //     return (
+        //       <article key={convertion.id} className="saved-convertion-card">
+        //         <p>{convertion.inputQuantity} {convertion.inputUnit} → {(convertion.outputQuantity).toFixed(2)} {convertion.outputUnit}</p>
+        //         <button onClick={() => dispatch(removeConvertion(convertion.id))}>X</button>
+        //       </article>
+        //     );
+        //   });
+        // };
+        
+  const [savedConvertions, setSavedConvertions] = useState([])
+
+  const deleteConvertionFromDB = (id) => {
+    axios.delete("api/convertions", {data:{id: id}})
+    location.reload()
+  }
+
+  const printCardsFromDB = () => {
+    return savedConvertions.map((convertion) => {
       return (
         <article key={convertion.id} className="saved-convertion-card">
           <p>{convertion.inputQuantity} {convertion.inputUnit} → {(convertion.outputQuantity).toFixed(2)} {convertion.outputUnit}</p>
-          <button onClick={() => dispatch(removeConvertion(convertion.id))}>X</button>
+          <button onClick={() => deleteConvertionFromDB(convertion.id)}>X</button>
         </article>
       );
     });
-  };
-  
+  }
+
+  useEffect(() => {
+    axios.get("api/convertions")
+      .then(response => setSavedConvertions(response.data))
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <section id="saved-convertions-container">
       {savedConvertions.length > 0 ? <p id="saved-title">saved</p> : ""}
       <div id="card-container">
-        {printCards()}
+        {printCardsFromDB()}
       </div>
     </section>
   );
